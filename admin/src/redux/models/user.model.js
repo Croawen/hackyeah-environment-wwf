8653d7ffdfe44d9f1fb7b1482a110e75fbaa3f66
Mateusz Chwedczuk <mateusz.chwedczuk@geekforce.io>
2019-09-15 03:15:44 +0200

    ...

--- admin/src/redux/models/user.model.js
+++ admin/src/redux/models/user.model.js
@@ -1,41 +1,38 @@
 import produce from "immer";
 import {
-    clearCurrentUserLS,
-    getCurrentUserLS,
-    setCurrentUserLS
+  clearCurrentUserLS,
+  getCurrentUserLS
 } from "@common/utils/localStorage/localStorage";
-import { logInUser } from "@user/user.api";
 import history from "../history";
 
 export const currentUserModel = {
-    state: {
-        currentUser: getCurrentUserLS()
+  state: {
+    currentUser: getCurrentUserLS()
+  },
+
+  reducers: {
+    setCurrentUser(state, payload) {
+      return produce(state, draft => {
+        draft.currentUser = payload;
+      });
     },
 
-    reducers: {
-        setCurrentUser(state, payload) {
-            return produce(state, draft => {
-                draft.currentUser = payload;
-            });
-        },
+    clearUser(state, payload) {
+      return produce(state, draft => {
+        draft.currentUser = null;
+      });
+    }
+  },
 
-        clearUser(state, payload) {
-            return produce(state, draft => {
-                draft.currentUser = null;
-            });
-        }
+  effects: dispatch => ({
+    async logIn({ login, password }) {
+      setCurrentUserLS({});
     },
 
-    effects: dispatch => ({
-        async logIn({ login, password }) {
-            const res = await logInUser({ login, password });
-            setCurrentUserLS(res);
-        },
-
-        logOut() {
-            clearCurrentUserLS();
-            this.clearUser();
-            return history.push("/login");
-        }
-    })
+    logOut() {
+      clearCurrentUserLS();
+      this.clearUser();
+      return history.push("/login");
+    }
+  })
 };
