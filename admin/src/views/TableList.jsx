49f8de9c54b496f1a756d2ebee6b664d7dd1be4b
Mateusz Chwedczuk <mateusz.chwedczuk@geekforce.io>
2019-09-15 07:04:14 +0200

    ...

--- admin/src/views/TableList.jsx
+++ admin/src/views/TableList.jsx
@@ -20,74 +20,72 @@ import { Grid, Row, Col, Table } from "react-bootstrap";
 
 import Card from "components/Card/Card.jsx";
 import { thArray, tdArray } from "variables/Variables.jsx";
+import wretch from "wretch";
+
+const api = {
+  noAuth: () => wretch().content("application/json")
+};
 
 class TableList extends Component {
+  state = {
+    items: null
+  };
+
+  async componentDidMount() {
+    let url = "http://localhost:4000";
+    if (process.env.API_URL) url = process.env.API_URL;
+
+    const res = await api
+      .noAuth()
+      .url(`${url}/admin-locations`)
+      .get()
+      .json();
+
+    if (res && res.items) {
+      this.setState({
+        items: res.items
+      });
+    }
+
+    console.log(res);
+  }
+
   render() {
     return (
       <div className="content">
         <Grid fluid>
           <Row>
             <Col md={12}>
-              <Card
-                title="Striped Table with Hover"
-                category="Here is a subtitle for this table"
-                ctTableFullWidth
-                ctTableResponsive
-                content={
-                  <Table striped hover>
-                    <thead>
-                      <tr>
-                        {thArray.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
+              {this.state.items && (
+                <Card
+                  title="Raporty"
+                  category="Lista zgłoszeń użytkowników"
+                  ctTableFullWidth
+                  ctTableResponsive
+                  content={
+                    <Table striped hover>
+                      <thead>
+                        <tr>
+                          {Object.keys(this.state.items[0]).map((prop, key) => {
+                            return <th key={key}>{prop}</th>;
+                          })}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.state.items.map(item => {
+                          return (
+                            <tr key={item.id}>
+                              {Object.keys(item).map(key => {
+                                return <td key={key}>{item[key]}</td>;
+                              })}
+                            </tr>
+                          );
                         })}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {tdArray.map((prop, key) => {
-                        return (
-                          <tr key={key}>
-                            {prop.map((prop, key) => {
-                              return <td key={key}>{prop}</td>;
-                            })}
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </Table>
-                }
-              />
-            </Col>
-
-            <Col md={12}>
-              <Card
-                plain
-                title="Striped Table with Hover"
-                category="Here is a subtitle for this table"
-                ctTableFullWidth
-                ctTableResponsive
-                content={
-                  <Table hover>
-                    <thead>
-                      <tr>
-                        {thArray.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
-                        })}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {tdArray.map((prop, key) => {
-                        return (
-                          <tr key={key}>
-                            {prop.map((prop, key) => {
-                              return <td key={key}>{prop}</td>;
-                            })}
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </Table>
-                }
-              />
+                      </tbody>
+                    </Table>
+                  }
+                />
+              )}
             </Col>
           </Row>
         </Grid>
