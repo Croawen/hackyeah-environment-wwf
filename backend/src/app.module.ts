62fbc5ded3456665f51bb286df33546e1687f2a9
Mateusz Chwedczuk <mateusz.chwedczuk@geekforce.io>
2019-09-15 04:31:15 +0200

    ...

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,9 @@ import { TypegooseModule } from "nestjs-typegoose";
 import { AppController } from "./app.controller";
 import { LocationModule } from "./modules/location/location.module";
 import { ReportModule } from "./modules/report/report.module";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import * as path from "path";
 import { FileModule } from "./modules/files/file.module";
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: path.join(__dirname, "..", "static"),
-    }),
     // @ts-ignore
     TypegooseModule.forRoot(process.env.MONGO_URL, {
       // @ts-ignore
